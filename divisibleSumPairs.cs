d39336b41c903e51542e9cd8656b0f074e3b0259
Nida KUL <79106903+nidakul@users.noreply.github.com>
2024-10-03 18:50:37 +0300

    Create divisibleSumPairs.cs

---
+++ divisibleSumPairs.cs
@@ -0,0 +1,13 @@
+  public static int divisibleSumPairs(int n, int k, List<int> ar)
+    {
+        int count = 0;
+        for(int i = 0; i<n; i++){
+            for(int j = i+1; j < n; j++){
+                int sum = ar[i] + ar[j];
+                if(sum % k == 0){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
