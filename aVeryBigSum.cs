579d5c5280f914b6396437e5479cf227835f7d5d
Nida KUL <79106903+nidakul@users.noreply.github.com>
2024-10-07 22:35:03 +0300

    Create aVeryBigSum.cs

---
+++ aVeryBigSum.cs
@@ -0,0 +1,8 @@
+ public static long aVeryBigSum(List<long> ar)
+    {
+        long sum = 0;
+        for(int i = 0; i<ar.Count; i++){
+            sum += ar[i];
+        }
+        return sum;
+    }
