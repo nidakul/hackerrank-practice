a7f2d4b855ea27f0016fec0f779a0a597ef0003b
Nida KUL <79106903+nidakul@users.noreply.github.com>
2024-10-07 22:28:19 +0300

    Create simpleArraySum.cs

---
+++ simpleArraySum.cs
@@ -0,0 +1,8 @@
+ public static int simpleArraySum(List<int> ar)
+    {
+        int sum = 0;
+        for(int i = 0; i<ar.Count; i++){
+            sum += ar[i]; 
+        }
+        return sum;
+    }
