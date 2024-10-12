f441feb9f3bed4c8d43560fbc937c37d6b507083
Nida KUL <79106903+nidakul@users.noreply.github.com>
2024-10-12 16:39:41 +0300

    Update and rename sockMerchant.cs to salesByMatch.cs

--- salesByMatch.cs
+++ salesByMatch.cs
@@ -1,7 +1,8 @@
  public static int sockMerchant(int n, List<int> ar)
     {
         int count = 0;
-        ar = ar.OrderBy(a => a).ToList();
+        //ar = ar.OrderBy(a => a).ToList();
+        ar.Sort();
         for(int i = 0; i<n-1; i++){
             if(ar[i] == ar[i+1]){
                 count++;
